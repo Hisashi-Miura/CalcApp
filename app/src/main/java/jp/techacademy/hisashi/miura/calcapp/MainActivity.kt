e1c13901116bd187c568c54b83d829b6dc99b5ed
Hisashi-Miura <hisashi@miura-farm.com>
2021-03-26 10:08:54 +0900

    一時修正バージョン

--- app/src/main/java/jp/techacademy/hisashi/miura/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hisashi/miura/calcapp/MainActivity.kt
@@ -1,75 +1,90 @@
 package jp.techacademy.hisashi.miura.calcapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+//import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import android.util.Log
+import android.content.Intent
+//import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
 
+    fun check():Boolean{
+        if (editText1.text.toString()=="" || editText2.text.toString()==""){
+            return true
+        } else {
+            return false
+        }
+    }
+    fun checkzero():Boolean{
+        if (editText2.text.toString()=="0"){
+            return true
+        } else {
+            return false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar))
 
-        var num1:Double=2.0
-        var num2:Double=3.0
-        var num3:Double=3.0
+        var num1:Double
 
         button1.setOnClickListener {
-
-            if (editText1.text.toString()=="" || editText2.text.toString()==""){
-                textView1.text="２つの値を入力してください。"
-            } else {
-                num1 = editText1.text.toString().toDouble()
-                num2 = editText2.text.toString().toDouble()
-                num3 = num1 + num2
-                textView1.text =num3.toString()
-            }
-
+            if (check() == true){
+                Snackbar.make(button1, "２つの値を入力してください。", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+                } else {
+                    num1 = editText1.text.toString().toDouble()+editText2.text.toString().toDouble()
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", num1)
+                    startActivity(intent)
+                }
         }
         button2.setOnClickListener {
-            if (editText1.text.toString()=="" || editText2.text.toString()==""){
-                textView1.text="２つの値を入力してください。"
+            if (check() == true){
+                Snackbar.make(button2, "２つの値を入力してください。", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
             } else {
-                num1 = editText1.text.toString().toDouble()
-                num2 = editText2.text.toString().toDouble()
-                num3 = num1 - num2
-                textView1.text =num3.toString()
+                num1 = editText1.text.toString().toDouble()-editText2.text.toString().toDouble()
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("VALUE1", num1)
+                startActivity(intent)
             }
         }
         button3.setOnClickListener {
-            if (editText1.text.toString()=="" || editText2.text.toString()==""){
-                textView1.text="２つの値を入力してください。"
+            if (check() == true){
+                Snackbar.make(button3, "２つの値を入力してください。", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
             } else {
-                num1 = editText1.text.toString().toDouble()
-                num2 = editText2.text.toString().toDouble()
-                num3 = num1 * num2
-                textView1.text =num3.toString()
+                num1 = editText1.text.toString().toDouble()*editText2.text.toString().toDouble()
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("VALUE1", num1)
+                startActivity(intent)
             }
         }
         button4.setOnClickListener {
-            if (editText1.text.toString()=="" || editText2.text.toString()==""){
-                textView1.text="２つの値を入力してください。"
-            } else {
-                num1 = editText1.text.toString().toDouble()
-                num2 = editText2.text.toString().toDouble()
-                num3 = num1 / num2
-                textView1.text =num3.toString()
+            if (check() == true){
+                Snackbar.make(button4, "２つの値を入力してください。", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            } else if(checkzero()==true) {
+                Snackbar.make(button4, "ゼロは入力しないでください。", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            } else{
+                num1 = editText1.text.toString().toDouble()/editText2.text.toString().toDouble()
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("VALUE1", num1)
+                startActivity(intent)
             }
         }
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -86,8 +101,4 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-
-    
-
-    
 }
---
+++ app/src/main/java/jp/techacademy/hisashi/miura/calcapp/SecondActivity.kt
@@ -0,0 +1,16 @@
+package jp.techacademy.hisashi.miura.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val value1 = intent.getDoubleExtra("VALUE1", 0.0)
+        textView3.text = "${value1}"
+    }
+}
